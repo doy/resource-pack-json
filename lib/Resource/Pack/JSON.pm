549b2365930137f85799ee1975b4b43dbdbdd290
Jesse Luehrs <doy@tozt.net>
2010-04-19 15:06:33 -0500

    initial commit

---
+++ lib/Resource/Pack/JSON.pm
@@ -0,0 +1,71 @@
+package Resource::Pack::JSON;
+use Moose;
+
+=head1 NAME
+
+Resource::Pack::JSON -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-resource-pack-json at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Resource-Pack-JSON>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Resource::Pack::JSON
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Resource-Pack-JSON>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Resource-Pack-JSON>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Resource-Pack-JSON>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Resource-Pack-JSON>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
